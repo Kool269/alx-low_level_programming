242e80c27694b1337d98ce0f7d4a192149772cd2
Nicholas Kool <koolnicholas001@gmail.com>
2022-07-06 06:53:30 -0700

    added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ * Return: always 0.
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
